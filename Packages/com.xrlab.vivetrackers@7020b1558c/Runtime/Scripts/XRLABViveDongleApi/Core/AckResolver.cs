0f184b625af923122849f72114d792f1ca421ab9
DESKTOP-IHQAPHV\User <s.blinov@atomgroup.io>
2024-06-28 12:53:13 +0300

    no message

---
+++ Packages/com.xrlab.vivetrackers@7020b1558c/Runtime/Scripts/XRLABViveDongleApi/Core/AckResolver.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VIVE_Trackers
+{
+    public class AckResolver
+    {
+        public AckResolver(DongleHID hid)
+        {
+             
+        }
+    }
+}
